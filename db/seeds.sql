a9a40b5aa02cc4eceab68251e6f8775bb65a3287
wmsal80 <wmsal80@gmail.com>
2021-03-04 17:59:27 -0500

    burger time

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO burgers (burger_name) VALUES ('Hamburger');
+INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Pizza Burger');
+INSERT INTO burgers (burger_name) VALUES ('Hangover Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheeseburger', true);
+
+SELECT * FROM burgers;
